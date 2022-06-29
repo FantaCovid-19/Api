856b74f9aedd12f0f508929ff9146edb95e4f531
MrFantasma <hugoalejandromoya@gmail.com>
2022-06-29 09:02:05 -0400

    Generate CreateUser

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,3 +1,5 @@
+import User from '../models/User'
+import Role from '../models/Role'
 
 export const getUser = (req, res) => {
 
@@ -7,8 +9,31 @@ export const getUserById = (req, res) => {
 
 }
 
-export const createUser = (req, res) => {
+export const createUser = async (req, res) => {
+  try {
+    const { username, email, password, roles } = req.body
+    const roleFound = await Role.find({ name: { $in: roles } })
 
+    const user = new User({
+      username,
+      email,
+      password,
+      roles: roleFound.map((role) => role._id)
+    })
+
+    user.password = await User.encryptPassword(user.password)
+
+    const savedUser = await user.save()
+
+    return res.status(200).json({
+      _id: savedUser._id,
+      username: savedUser.username,
+      email: savedUser.email,
+      roles: savedUser.roles
+    })
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 export const updateUser = (req, res) => {
