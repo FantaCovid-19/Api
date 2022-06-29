1127000b981f2480022a92ca3d8b5fa6793d690e
MrFantasma <hugoalejandromoya@gmail.com>
2022-06-28 22:28:58 -0400

    Modified Auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,4 +1,18 @@
 import { Router } from 'express'
 const router = Router()
 
+import * as authCtrl from '../controller/auth.controller'
+import { verifySignUp } from '../middlewares'
+
+router.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Headers',
+    'x-access-token, Origin, Content-Type, Accept'
+  )
+  next()
+})
+
+router.post('/signup', [verifySignUp.checkDuplicateDataUser, verifySignUp.checkRoleExisted], authCtrl.singUp)
+router.post('/signin', authCtrl.singIn)
+
 export default router
